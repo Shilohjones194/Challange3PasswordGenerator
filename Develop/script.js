f17a7fc27a6fab33436b9862adc35b71f41b30ba
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-04-04 19:47:00 -0700

    Adding detailed //notes

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,13 @@ var randomCharacters = [
 ]
 
 function generatePassword() {
+  //Prompt 1- length of password//
   var length = Number(prompt("Before you we generate a password, we need to ask a few questions. How long would you like it to be? Choose between 8 to 128 characters"));
-
+  
+  ///Alert 1- Needs to be 8-128 characters///
   while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("It must be between 8 and 128 characters"));
 
+  ///Lines 14-29 randomCharacters confirmations looped in with line 3///
   var uppercase = confirm("Do you want uppercase letters in your password?");
   if (uppercase) {
     randomCharacters.push(getRandomUppercase); //do it for each VAR//
@@ -25,11 +28,12 @@ function generatePassword() {
     randomCharacters.push(getRandomSpecialCharacters);
   }
 
+/// Lines 32-50 confirmation in While Loop///
   while (!uppercase && !lowercase && !numbers && !specialCharacters) {
     alert("You must select at least one character type");
     var uppercase = confirm("Do you want uppercase letters in your password?");
     if (uppercase) {
-      randomCharacters.push(getRandomUppercase); //do it for each VAR//
+      randomCharacters.push(getRandomUppercase); 
     }
     var lowercase = confirm("Do you want lowercase letters?");
     if (lowercase) {
@@ -44,7 +48,7 @@ function generatePassword() {
       randomCharacters.push(getRandomSpecialCharacters);
     }
   };
-
+ /////Returning Password (this is what makes it random///
   var password = "";
   for (var i = 0; i < length; i++) {
     password += randomCharacters[Math.floor(Math.random() * randomCharacters.length)]()
@@ -66,6 +70,7 @@ function getRandomSpecialCharacters() {
   var specialCharacters = '!@#$%^&*';
   return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 }
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
