a9cca00b3bcfb92deae4fea84b4178ffbcb582cc
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-04-04 19:36:59 -0700

    adjusting folders

---
+++ Develop/script.js
@@ -0,0 +1,76 @@
+var generateBtn = document.querySelector("#generate");
+
+var randomCharacters = [
+]
+
+function generatePassword() {
+  var length = Number(prompt("Before you we generate a password, we need to ask a few questions. How long would you like it to be? Choose between 8 to 128 characters"));
+
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("It must be between 8 and 128 characters"));
+
+  var uppercase = confirm("Do you want uppercase letters in your password?");
+  if (uppercase) {
+    randomCharacters.push(getRandomUppercase); //do it for each VAR//
+  }
+  var lowercase = confirm("Do you want lowercase letters?");
+  if (lowercase) {
+    randomCharacters.push(getRandomLowercase);
+  }
+  var numbers = confirm("Do you want to use numbers in your password?");
+  if (numbers) {
+    randomCharacters.push(getRandomNumbers);
+  }
+  var specialCharacters = confirm("Do you want to use Special characters?");
+  if (specialCharacters) {
+    randomCharacters.push(getRandomSpecialCharacters);
+  }
+
+  while (!uppercase && !lowercase && !numbers && !specialCharacters) {
+    alert("You must select at least one character type");
+    var uppercase = confirm("Do you want uppercase letters in your password?");
+    if (uppercase) {
+      randomCharacters.push(getRandomUppercase); //do it for each VAR//
+    }
+    var lowercase = confirm("Do you want lowercase letters?");
+    if (lowercase) {
+      randomCharacters.push(getRandomLowercase);
+    }
+    var numbers = confirm("Do you want to use numbers in your password?");
+    if (numbers) {
+      randomCharacters.push(getRandomNumbers);
+    }
+    var specialCharacters = confirm("Do you want to use Special characters?");
+    if (specialCharacters) {
+      randomCharacters.push(getRandomSpecialCharacters);
+    }
+  };
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += randomCharacters[Math.floor(Math.random() * randomCharacters.length)]()
+  }
+  return password
+}
+
+// generating functions fromCharCode:   https://www.w3schools.com/charsets/ref_html_ascii.asp
+function getRandomUppercase() { //*() * (Max- min) + min
+  return String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
+}
+function getRandomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * (122 - 97) + 97));
+}
+function getRandomNumbers() {
+  return String.fromCharCode(Math.floor(Math.random() * (57 - 48) + 48));
+}
+function getRandomSpecialCharacters() {
+  var specialCharacters = '!@#$%^&*';
+  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+}
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+generateBtn.addEventListener("click", writePassword)
